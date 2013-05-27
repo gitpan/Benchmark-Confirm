ab80ee9f99e55de9a9ceeb8cf8e9e59d9c6f99d5
Dai Okabayashi <bayashi@cpan.org>
2013-05-27 22:50:43 +0000

    Import of BAYASHI/Benchmark-Confirm-0.03 from CPAN.
    gitpan-cpan-distribution: Benchmark-Confirm
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BAYASHI/Benchmark-Confirm-0.03.tar.gz
    gitpan-cpan-author:       BAYASHI
    gitpan-cpan-maturity:     released

--- cpanfile
+++
@@ -1,3 +0,0 @@
-requires 'Benchmark', '1.051';
-requires 'Test::More', '0.88';
-requires 'perl', 'v5.8.1';
--- lib/Benchmark/Confirm.pm
+++ lib/Benchmark/Confirm.pm
@@ -2,7 +2,7 @@ package Benchmark::Confirm;
 use strict;
 use warnings;
 
-our $VERSION = '0.22';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -134,6 +134,7 @@ END {
 
 sub atonce {
     require Test::More;
+    Test::More->import;
 
     my $expect = _normalize(shift @CONFIRMS);
     Test::More::ok(1);
